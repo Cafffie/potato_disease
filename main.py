806030d0de01e6462350b94b84162e3cbd4f2630
Cafffie <143073291+Cafffie@users.noreply.github.com>
2024-04-02 11:58:35 +0100

    main_code

---
+++ main.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+from tensorflow.keras import models
+import matplotlib.pyplot as plt
+import streamlit as st
+from PIL import Image, ImageOps
+import numpy as np
+
+
+model= tf.keras.models.load_model(r"C:\Users\Open user\Documents\potato_disease\models\1")
+class_names= ['Early_blight', 'Late_blight', 'healthy']
+image_size= 256
+
+def predict_image(model, img):
+    image_arr = image.resize((image_size, image_size))
+    image_arr= tf.keras.preprocessing.image.img_to_array(img)
+    image_arr= tf.expand_dims(image_arr, 0)
+    
+    predictions= model.predict(image_arr)
+    predicted_class= class_names[np.argmax(predictions[0])]
+    confidence= round(100*(np.max(predictions[0])), 2)
+    
+    return predicted_class, confidence
+
+#Streamlit app
+file_upload= st.file_uploader("upload an image", type= ["jpg", "png", "jpeg"])
+if file_upload is not None:
+    image= Image.open(file_upload)
+    st.image(image, use_column_width=True)
+    predicted_class, confidence = predict_image(model, image)
+    st.success(f"Prediction: {predicted_class}.\n Confidence: {confidence}%")
