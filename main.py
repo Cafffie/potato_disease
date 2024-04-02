29187d63cc42363742f0a3dfd099537a03f16740
Cafffie <143073291+Cafffie@users.noreply.github.com>
2024-04-02 13:52:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -33,8 +33,8 @@ def predict_image(model, img):
     return predicted_class, confidence
 
 #Streamlit app
-st.write(author: Arebi Olawunmi)
-st.write(Date: 24/12/2024)
+st.write("author: Arebi Olawunmi")
+st.write("Date: 24/12/2024")
 
 st.title("Potato Disease Detection App")
 file_upload= st.file_uploader("Upload a potato plant leave to test for a disease.", 
